b2f7b9a232a448f4acef3a7c746443bcde1eee0b
Ashw12 <86472085+Ashw12@users.noreply.github.com>
2022-07-14 15:49:38 +0530

    first commit

---
+++ Count_Digits.java
@@ -0,0 +1,20 @@
+package Lecture4_Assignment;
+
+import java.util.Scanner;
+
+public class Count_Digits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		while(n!=0) {
+			count++;
+			n=n/10;
+			
+		}
+		System.out.println(count);
+	   }
+
+}
